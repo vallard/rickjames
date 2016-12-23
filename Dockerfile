d68ddbb93d4de6cb2167814cbc72a00f9a94a27e
vallard <vallard@benincosa.com>
2016-12-23 12:44:08 -0800

    make rickjames go

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,9 @@
-FROM python:2-alpine
-MAINTAINER Hank Preston <hank.preston@gmail.com>
-EXPOSE 5000
+FROM alpine:3.4
 
-# Install basic utilities
-RUN apk add -U \
-        ca-certificates \
-  && rm -rf /var/cache/apk/* \
-  && pip install --no-cache-dir \
-        setuptools \
-        wheel
+RUN apk update && \
+  apk add \
+    ca-certificates && \
+  rm -rf /var/cache/apk/*
 
-
-COPY requirements.txt /app/
-RUN pip install -r /app/requirements.txt
-
-WORKDIR /app
-ADD ./bot /app/bot
-
-CMD [ "python", "bot/bot.py" ]
+ADD drone-spark /bin/
+ENTRYPOINT ["/bin/drone-spark"]
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/urfave/cli"
+	"github.com/vallard/spark"
+)
+
+type BotConfig struct {
+	Token    string
+	Keyword  string
+	Response string
+	RoomId   string
+}
+
+var build = "1"
+var bot BotConfig
+
+func getMessage(data map[string]interface{}) spark.Message {
+	var m spark.Message
+	for k, v := range data {
+		// make sure the value is of type string.
+		if reflect.TypeOf(v) == reflect.TypeOf("") {
+			vv := v.(string)
+			switch k {
+			case "id":
+				m.Id = vv
+			case "roomId":
+				m.RoomId = vv
+			case "roomType":
+				m.RoomType = vv
+			case "text":
+				m.Text = vv
+			case "personId":
+				m.PersonId = vv
+			case "personEmail":
+				m.PersonEmail = vv
+			case "markdown":
+				m.Markdown = vv
+			case "html":
+				m.Html = vv
+			case "created":
+				tt, err := time.Parse("2006-01-02T03:04:05+00:00", vv)
+				if err == nil {
+					m.Created = tt
+				}
+			default:
+				log.Printf("unknown key: %s\n", k)
+			}
+		}
+	}
+	return m
+}
+
+func sendHello() error {
+	// create a new spark client
+	s := spark.New(bot.Token)
+
+	// create a new message
+	newMessage := spark.Message{
+		RoomId: bot.RoomId,
+		Text:   bot.Response,
+	}
+	_, err := s.CreateMessage(newMessage)
+	return err
+}
+
+func handleWebhook(w spark.Webhook) {
+	// see if there is a message with this spark webhook.
+	message := getMessage(w.Data)
+	bot.RoomId = message.RoomId
+	if strings.Contains(message.Text, bot.Keyword) {
+		err := sendHello()
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "sparkbot maker"
+	app.Usage = "spark bot maker"
+	app.Action = run
+	app.Version = fmt.Sprintf("0.%s", build)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "token",
+			Usage:  "token of spark bot",
+			EnvVar: "SPARK_TOKEN",
+		},
+		cli.StringFlag{
+			Name:   "keyword",
+			Usage:  "keyword to look for",
+			EnvVar: "KEYWORD",
+		},
+		cli.StringFlag{
+			Name:   "response",
+			Usage:  "response you want for when keyword is found",
+			EnvVar: "RESPONSE",
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	bot.Token = c.String("token")
+	bot.Keyword = "hello"
+	bot.Response = "hello back to you my friend!"
+	if c.String("keyword") != "" {
+		bot.Keyword = c.String("keyword")
+	}
+	if c.String("response") != "" {
+		bot.Response = c.String("response")
+	}
+
+	http.HandleFunc("/spark-hook", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			dec := json.NewDecoder(r.Body)
+			for {
+				var wh spark.Webhook
+				if err := dec.Decode(&wh); err == io.EOF {
+					break
+				} else if err != nil {
+					log.Println(err)
+				}
+				// do something with the message.
+				handleWebhook(wh)
+			}
+		}
+		fmt.Fprintf(w, "Thanks for playing, %q\n", r.RequestURI)
+	})
+
+	return http.ListenAndServe(":8080", nil)
+}
